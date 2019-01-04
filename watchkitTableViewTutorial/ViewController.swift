47a8c798b972b8b235dd764a819428babe386c51
Amber Medina <alouiseme@gmail.com>
2019-01-04 08:57:11 -0600

    Initial Commit

---
+++ watchkitTableViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  watchkitTableViewTutorial
+//
+//  Created by Amber on 1/4/19.
+//  Copyright © 2019 Amber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
